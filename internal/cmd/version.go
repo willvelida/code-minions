8ebd2249d2c565b5c80be8b6ce91dfc69caf1fa6
velidawill <velidawill@microsoft.com>
2026-02-12 18:19:02 +1100

    fix(version): use runtime/debug to read embedded module version
    The version command was hardcoded to 'dev', which was never overridden at runtime. Replace the static variable with debug.ReadBuildInfo() to read the module version that Go automatically embeds when installed via go install.
    
    Signed-off-by: velidawill <velidawill@microsoft.com>

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -2,18 +2,30 @@ package cmd
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
 
-var Version = "dev"
+func getVersion() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "dev"
+	}
+
+	if info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+
+	return "dev"
+}
 
 func newVersionCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print the version of code-minions",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("code-minions %s\n", Version)
+			fmt.Printf("code-minions %s\n", getVersion())
 		},
 	}
 }
